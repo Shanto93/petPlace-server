a02ea371754c20b6af94be614045366ff5df59f8
Shanto93 <shantoislam7363@gmail.com>
2026-04-26 22:22:46 +0600

    update

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,3 +1,18 @@
+// import "dotenv/config";
+// import { defineConfig } from "prisma/config";
+
+// export default defineConfig({
+//   schema: "./prisma/schema",
+//   migrations: {
+//     path: "prisma/migrations",
+//   },
+//   datasource: {
+//     url: process.env["DATABASE_URL"],
+//   },
+// });
+
+/// <reference types="node" />
+
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
